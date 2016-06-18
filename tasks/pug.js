59811e119a024da82598ae8ca3ff269a50eb2da4
Melle Wynia <contact@mellewynia.nl>
2016-06-18 19:38:40 +0200

    Added a comment to pug task explaining the commented code

--- tasks/pug.js
+++ tasks/pug.js
@@ -9,6 +9,12 @@ import pug from 'gulp-pug';
 
 export default () => {
 
+    //
+    // You can easily uncomment the notifications - these
+    // can be handy when your pug tasks takes long (> 7 sec)
+    // and it starts to be handy to be informed when it’s ready.
+    //
+
     return gulp.src( './'+config.paths.source+'/pug/*.pug' )
         //.pipe(notify({ icon:false, onLast:false, title:'Processing pug', message: 'Started processing changes in pug' }))
         .pipe(pug({
